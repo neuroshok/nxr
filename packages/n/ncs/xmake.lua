8ca64580a5d1920e5656a4915982a43a3755ce46
Arkena <ads.neuroshok@gmail.com>
2022-06-13 18:04:20 +0200

    [fix] versions

--- packages/n/ncs/xmake.lua
+++ packages/n/ncs/xmake.lua
@@ -3,7 +3,7 @@ package("ncs")
 	add_urls("https://github.com/neuroshok/ncs")
 	add_urls("https://github.com/arkena00/ncs")
 
-	add_versions("latest", "462c36525b9ddf4bb744214d1f72b8a139b2a3f2")
+	add_versions("0.0.1", "462c36525b9ddf4bb744214d1f72b8a139b2a3f2")
 
 	on_install(function (package)
 		import("package.tools.xmake").install(package)
