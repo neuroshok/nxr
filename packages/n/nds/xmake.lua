507efe93681d302315e02603ee3070d9c433f6ff
Arkena <ads.neuroshok@gmail.com>
2022-06-14 00:30:12 +0200

    [fix] ncs version

--- packages/n/nds/xmake.lua
+++ packages/n/nds/xmake.lua
@@ -3,7 +3,7 @@ package("nds")
 	add_urls("https://github.com/neuroshok/nds.git")
 	add_urls("https://github.com/arkena00/nds.git")
 
-	add_versions("0.0.1", "8acedc2f985c6866d11b2c71d615f4a1343c763c")
+	add_versions("0.0.1", "ab85c52f05b8af9ae3550f95e3d25a364b1bd835")
 
 	on_install(function (package)
 		import("package.tools.xmake").install(package)
