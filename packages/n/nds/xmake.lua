e969a9c7e7986815d947c944b14eb5b5ab6345a9
Arkena <ads.neuroshok@gmail.com>
2022-06-14 01:28:26 +0200

    [fix] nds url

--- packages/n/nds/xmake.lua
+++ packages/n/nds/xmake.lua
@@ -1,7 +1,7 @@
 package("nds")
 
 	add_urls("https://github.com/neuroshok/nds.git")
-	add_urls("https://github.com/arkena00/nds")
+	add_urls("https://github.com/arkena00/nds.git")
 
 	add_versions("0.0.1", "8acedc2f985c6866d11b2c71d615f4a1343c763c")
 
