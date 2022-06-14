f6ab044316fa4482485a1122c05045249c330a71
Arkena <ads.neuroshok@gmail.com>
2022-06-14 19:34:54 +0200

    [update] nds

--- packages/n/nds/xmake.lua
+++ packages/n/nds/xmake.lua
@@ -3,7 +3,7 @@ package("nds")
 	add_urls("https://github.com/neuroshok/nds.git")
 	add_urls("https://github.com/arkena00/nds.git")
 
-	add_versions("0.0.1", "acc4c9a128d0aa4243d7b0e0bef17c8ccf303642")
+	add_versions("0.0.1", "b6f46fd6b078088222c388e26eb068a682c7ad38")
 
 	on_install(function (package)
 		import("package.tools.xmake").install(package)
