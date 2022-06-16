407430c2e5da1c89069a86b4538df970a078c0bc
Arkena <ads.neuroshok@gmail.com>
2022-06-16 02:19:40 +0200

    [update] ncs

--- packages/n/ncs/xmake.lua
+++ packages/n/ncs/xmake.lua
@@ -3,7 +3,7 @@ package("ncs")
 	add_urls("https://github.com/neuroshok/ncs.git")
 	add_urls("https://github.com/arkena00/ncs.git")
 
-	add_versions("0.0.1", "7ad8b69ed69dbf7efae2532cf7c7e91fbf31cde2")
+	add_versions("0.0.1", "63e38b97b2b6c0318fa79b0f967925be100ec999")
 
 	set_kind("library", { headeronly = true })
 
